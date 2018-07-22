ee6d67c5eabec1603bb41a52b9d3ee63016d5c76
Laura <s0548106@htw-berlin.de>
2018-07-22 22:48:43 +0200

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,3 @@ Auth::routes();
 Route::resource('todo', 'TodoController');
 Route::resource('category', 'CategoryController');
 Route::post('todo/{id}/updateStatus','TodoController@updateStatus')->name('updateStatus');
-/*Route::middleware(['auth'])->group(function () {
-  
-    Route::get('/','TodoController@index');
-    Route::get('/store','TodoController@store')->name('store');
-    Route::get('/edit/{id}','TodoController@edit')->name('edit');
-    Route::get('/delete/{id}','TodoController@delete')->name('delete');
-    Route::get('/update/{id}','TodoController@update')->name('update');
-    Route::get('/updateStatus/{id}','TodoController@updateStatus')->name('updateStatus');
- 
- 
- 
- });
- Auth::routes();*/
